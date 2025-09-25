41bf0436af51220063441b8b04b0ad8912afb9f7
llenax <berkebs18@proton.me>
2025-09-25 04:45:27 +0300

    gnome: remove useless software

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   environment.sessionVariables = {
     TMPDIR = "/tmp";
@@ -6,4 +7,21 @@
     EDITOR = "nvim";
     VISUAL = "nvim";
   };
+  environment.gnome.excludePackages = (with pkgs; [
+      atomix # puzzle game
+      cheese # webcam tool
+      epiphany # web browser
+      evince # document viewer
+      geary # email reader
+      gedit # text editor
+      gnome-characters
+      gnome-music
+      gnome-photos
+      gnome-terminal
+      gnome-tour
+      hitori # sudoku game
+      iagno # go game
+      tali # poker game
+      totem # video player
+  ]);
 }
