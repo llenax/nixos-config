28f0b697538e492b85542b48a8fd6b5a547b587b
llenax <berkebs18@proton.me>
2025-12-07 03:02:38 +0300

    dns: just use nm config

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -1,3 +1,4 @@
+{ config, pkgs, ... }:
 {
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -7,35 +8,9 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  services.resolved = {
-    enable = true;
-    dnssec = "allow-downgrade";
-    extraConfig = ''
-      DNS=1.1.1.1 1.0.0.1
-      FallbackDNS=
-      Domains=~.
-    '';
-  };
   networking = {
     networkmanager = {
       enable = true;
-      dns = "systemd-resolved";
-      settings = {
-        connection = {
-          "ipv4.ignore-auto-dns" = true;
-          "ipv6.ignore-auto-dns" = true;
-        };
-        ipv4 = {
-          method = "auto";
-          never-default = false;
-          may-fail = false;
-        };
-        ipv6 = {
-          method = "auto";
-          never-default = false;
-          may-fail = false;
-        };
-      };
     };
   };
 }
