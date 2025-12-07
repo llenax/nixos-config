8c0df62cfea12ce69328036d9c7aee5d1d75dcf9
llenax <berkebs18@proton.me>
2025-12-07 18:06:52 +0300

    portmaster: devmode enabled

--- nixos/modules/portmaster.nix
+++ nixos/modules/portmaster.nix
@@ -4,7 +4,7 @@
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "/home/berke/safing/portmaster/portmaster-start core";
+      ExecStart = "/home/berke/safing/portmaster/portmaster-start core -- -devmode";
       WorkingDirectory = "/home/berke";
       Restart = "always";
       Environment = "PATH=/run/current-system/sw/bin:/usr/bin:/bin";
